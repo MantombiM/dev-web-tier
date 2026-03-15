790ca97d3ed772b51c62215dc8ccc208931dfe3a
MantombiM <mimimanqele13@gmail.com>
2026-03-15 16:34:08 +0200

    Create Terraform directory structure with modules

---
+++ terraform/modules/network/main.tf
@@ -0,0 +1,12 @@
+# Network Module - VPC, Subnets, NAT Gateway, Security Groups
+# To be implemented in Phase 3
+
+# Resources to be created:
+# - aws_vpc - Main VPC (10.0.0.0/16)
+# - aws_subnet - Public subnets in us-east-1a and us-east-1b
+# - aws_subnet - Private subnets in us-east-1a and us-east-1b
+# - aws_internet_gateway - Internet gateway
+# - aws_nat_gateway - NAT gateway in us-east-1a
+# - aws_eip - Elastic IP for NAT gateway
+# - aws_route_table - Route tables for public and private subnets
+# - aws_security_group - ALB and application security groups
